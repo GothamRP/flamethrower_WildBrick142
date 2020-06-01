445e5b20cea4dee333f750d6e429cdac8342c9e3
rjmxtechnology <rjmxtechnology@gmail.com>
2020-05-31 22:26:03 -0500

    Update fxmanifest.lua
    fix it from my smartness

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,25 +6,6 @@ description_cont 'Repackaged from the dlc.rpf and made so it can be streamed to 
 
 original_author 'WildBrick142'
 original_mod 'https://www.gta5-mods.com/weapons/flamethrower-add-on-hud-icon'
-credits '
--Wildbrick142 (gta5-mods): Original mod creator.
--Rockstar Games: Flamethrower model.
--dexyfex/CodeWalker: Ability to edit ptfx files.
-'
-
-credits_stuff '
-You are allowed to:
--Use it on FiveM and whatever
--Reupload it elsewhere
-as long as you give credit to WildBrick142 
-and everyone listed in the credits section.
-
-You are NOT allowed to:
--Use data from this mod for your mod
-without asking me first and giving credit 
-to me and everyone listed in the credits section. 
-Please have some common courtesy.
-'
 
 resource_creator 'Success'
 resource_creator_link 'https://forum.cfx.re/u/succ3ssgaming/'
